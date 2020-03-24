25e989d4a3ec0ee1265fa81f7de0dfcc51d79049
smkattoula <60168409+smkattoula@users.noreply.github.com>
2020-03-24 16:00:03 -0700

    Fix Testing Issue

--- src/pages/__test__/HeroIndex.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import HeroIndex from '../HeroIndex'
-
-Enzyme.configure({ adapter: new Adapter() })
